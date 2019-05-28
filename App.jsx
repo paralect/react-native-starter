46de7d7aacabbba6478a8ff61e8198df407fea95
Nikita Solosnuk <n.solosnuk@paralect.com>
2019-05-28 20:32:30 +0300

    Implement eslint. Fix elsint errors

--- App.jsx
+++ App.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Config from 'react-native-config'
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 export default class App extends React.Component {
   render() {
     return (
@@ -12,12 +21,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
