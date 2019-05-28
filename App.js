6a3a16cca33787a9adea3445c6f99938678f44eb
Nikita Solosnuk <n.solosnuk@paralect.com>
2019-05-28 19:13:02 +0300

    Implement configuration management

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Config from 'react-native-config'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>The app is in {Config.MODE} mode!</Text>
       </View>
     );
   }
